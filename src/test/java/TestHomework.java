7502e3d11c8b3342f9b346ca34fe011040604bda
acostjen1978 <131210089+acostjen1978@users.noreply.github.com>
2023-04-25 18:00:54 -0400

    Jenniffer Acosta's Homework16

--- src/test/java/TestHomework.java
+++ src/test/java/TestHomework.java
@@ -1,6 +1,28 @@
+import org.openqa.selenium.chrome.ChromeOptions;
+
 public class TestHomework extends BaseTest{
 
+
+    @Test
     public void testMethod() {
 
-    }
-}
+
+                ChromeOptions options = new ChromeOptions();
+                options.addArguments("--remote-allow-origins=*");
+
+                WebDriver driver = new ChromeDriver(options);
+                driver.manage() .timeouts() .implicitlyWait(Durantion.ofSeconds(10));
+
+                String url = "http://bbb.testpro.io/";
+                driver.get(url);
+
+                WebElement clickRegistration = driver.findElement(By.cssSelector("registration.submit"));
+
+                Assert.assertTrue(driver.getCurrentUrl("https://bbb.testpro.io/registration.php"), url);
+                driver.quit();
+
+            }
+
+        }
+
+
