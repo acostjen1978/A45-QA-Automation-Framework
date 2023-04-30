8093e8a4d305b51a925d948b398b913fc5f25c45
acostjen1978 <131210089+acostjen1978@users.noreply.github.com>
2023-04-30 18:07:16 -0400

    Jenniffer Acosta's Homework17

--- src/test/java/TestHomework.java
+++ src/test/java/TestHomework.java
@@ -5,6 +5,34 @@ public class TestHomework extends BaseTest{
 
     @Test
     public void testMethod() {
+        provideEmail("demo@class.com");
+        providePassword("te$tStudent");
+        clickSubmit();
+        Thread.sleep(millis: 2000);
+
+        WebElement search = driver.findElement(By.cssSelector("[type = 'search']"));
+        search.sendKeys(...keysToSend: "Pluto");
+
+        WebElement viewAll = driver.findElement(By.cssSelector("[div.results h1 > button']"));
+        viewAll.click();
+        Thread.sleep(millis: 2000);
+
+        WebElement firstSong = driver.findElement(By.cssSelector("section#songResultsWrapper td.title"));
+        firstSong.click();
+        Thread.sleep(millis: 1000);
+
+        WebElement addToButton = driver.findElement(By.cssSelector("button[title= 'Add selected songs to...']"));
+        addToButton.click();
+        Thread.sleep(millis: 1000);
+
+        WebElement favoritePlaylist = driver.findElement(By.cssSelector("section#songResultsWrapper li.favorites"));
+        favoritesPlaylist.click();
+
+
+
+
+
+
 
 
                 ChromeOptions options = new ChromeOptions();
